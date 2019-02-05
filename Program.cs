f6370063ce6c0bb41b76c6303c9f85550f0a0b49
GauravSharma <gaurav.sharma1@thomsonreuters.com>
2019-02-05 13:08:16 +0530

    Setting up new project for Kafka chat messanger

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace Git_Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "This is my test information".
+            Message msg = new Message();
+
+        }
+    }
+}
