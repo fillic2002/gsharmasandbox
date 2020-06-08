e7a6db2746d7d183944c14d41dbb226f699d003d
Gaurav Sharma <fillic2002@gmail.com>
2020-06-08 11:37:45 +0530

    Create Entity.cs

---
+++ DailyRunJob/Entity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equity
+{
+	public class CompanySearchEntity
+	{
+		public Suggest obj; 
+
+	}
+	public class Suggest
+	{
+		public object link_src;
+		public object link_track;
+		public object pdt_dis_nm;
+		public object sc_id;
+		public object stock_name;
+		public object sc_sector_id;
+		public object sc_sector;
+	}
+}
